e17a6fec48321d37b0b0bbc1e17ce74694111433
unknown <ronalddelagracia@gmail.com>
2018-12-23 18:34:55 +0800

    commit beta 6pm dec2018

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
         <NavBar /> {/* this is the navigation */}
         <menuDrawer /> {/* this is the drawer */}
         {/* */}
+        {/* */}
         {/* develop a main Form */}
         {/* develop footer */}
         {/* develop a delivery Form */}
